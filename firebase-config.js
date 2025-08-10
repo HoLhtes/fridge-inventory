e2434950c3b3895b5804cc03ddeefce113224264
HoLhtes <166899452+HoLhtes@users.noreply.github.com>
2025-08-10 17:43:50 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@
 Example:
 <script>
   window.FIREBASE_CONFIG = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-app.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-app.appspot.com",
-    messagingSenderId: "...",
-    appId: "..."
+    apiKey: "AIzaSyDlHYUJkRySvtsirs1JkwG3vF6xGB8DB4s",
+    authDomain: "fridge-inventory-78cdc.firebaseapp.com",
+    projectId: "fridge-inventory-78cdc",
+    storageBucket: "fridge-inventory-78cdc.firebasestorage.app",
+    messagingSenderId: "265594761085",
+    appId: "1:265594761085:web:4205995ce6db2ae22fa614"
   };
-  window.FR_USER_ID = "your-unique-userid"; // optional - default is 'public-user'
+  window.FR_USER_ID = "my-fridge-user"; // optional - default is 'public-user'
 </script>
 */
